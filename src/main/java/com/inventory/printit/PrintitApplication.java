8fb7eec5abfad2a447f989657a7b44cbca63619a
emcb-ekanayaka <ekanayakabdr@gmail.com>
2024-12-07 11:33:51 +0530

    CI/CD pipeleine

---
+++ src/main/java/com/inventory/printit/PrintitApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.printit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventory/printit/PrintitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.printit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
