289c87adfa8fcf851e348d335b24a134e50d71c9
emcb-ekanayaka <ekanayakabdr@gmail.com>
2024-12-08 20:48:50 +0530

    Jenkins Test commit 2.

--- src/main/java/com/inventory/printit/PrintitApplication.java
+++ src/main/java/com/inventory/printit/PrintitApplication.java
@@ -8,8 +8,9 @@ public class PrintitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrintitApplication.class, args);
+		System.out.println("Hello this is Inventory Project 1");
 		System.out.println("Hello this is Inventory Project 2");
-		System.out.println("Hello this is Inventory Project 2");
+		System.out.println("Hello this is Inventory Project 3");
 	}
 
 }
