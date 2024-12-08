d0eb7f41e168e14ea1cde3c20b17e3d4edc6fd57
emcb-ekanayaka <ekanayakabdr@gmail.com>
2024-12-08 20:38:56 +0530

    Sout updated.

--- src/main/java/com/inventory/printit/PrintitApplication.java
+++ src/main/java/com/inventory/printit/PrintitApplication.java
@@ -8,7 +8,7 @@ public class PrintitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrintitApplication.class, args);
-		System.out.println("Hello this is Inventory Project");
+		System.out.println("Hello this is Inventory Project 2");
 	}
 
 }
