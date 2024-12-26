f4817f6fd3be7ef5fc51c90dfb8825f7f20a36c0
emcb-ekanayaka <ekanayakabdr@gmail.com>
2024-12-26 22:40:51 +0530

    Company and warehouse Duplicate function, Created.

--- src/main/java/com/inventory/printit/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/inventory/printit/service/impl/CompanyServiceImpl.java
@@ -40,26 +40,30 @@ public class CompanyServiceImpl implements CompanyService{
 
     @Override
     public CommonResponseDto saveCompany(RequestRegistryDto dto) {
-        System.out.println("IMPL 1 Data -- " + dto);
-        try {
-            String companyGenerateNum = "IBM-C" + "-" + generator.generateFourNumbers();
-            String comId =  generator.generateFourNumbers();
-            CompanyDto companyDto = new CompanyDto(
-                    comId,
-                    companyGenerateNum,
-                    dto.getCompanyName(),
-                    dto.getComAddressOne(),
-                    dto.getComAddressTwo(),
-                    dto.getComAddressThree(),
-                    dto.getBrNumber()
-            );
-            System.out.println("IMPL 2 Data -- " + companyDto);
-            System.out.println(companyMapper.dtoToCompanyEntity(companyDto));
-            companyRepo.save(companyMapper.dtoToCompanyEntity(companyDto));
+        Optional<Company> company = companyRepo.findCompanyByName(dto.getCompanyName());
+        if (!company.isPresent()){
+            try {
+                String companyGenerateNum = "IBM-C" + "-" + generator.generateFourNumbers();
+                String comId =  generator.generateFourNumbers();
+                CompanyDto companyDto = new CompanyDto(
+                        comId,
+                        companyGenerateNum,
+                        dto.getCompanyName(),
+                        dto.getComAddressOne(),
+                        dto.getComAddressTwo(),
+                        dto.getComAddressThree(),
+                        dto.getBrNumber()
+                );
+                System.out.println("IMPL 2 Data -- " + companyDto);
+                System.out.println(companyMapper.dtoToCompanyEntity(companyDto));
+                companyRepo.save(companyMapper.dtoToCompanyEntity(companyDto));
 
-            return new CommonResponseDto(201, "Company  saved!", companyDto.getCompanyCode(), new ArrayList<>());
-        }catch (Exception e){
-            throw new EntryNotFoundException("Can't Save because of this Error -->  " + e);
+                return new CommonResponseDto(201, "Company  saved!", companyDto.getCompanyCode(), new ArrayList<>());
+            }catch (Exception e){
+                throw new EntryNotFoundException("Can't Save because of this Error -->  " + e);
+            }
+        }else {
+            return new CommonResponseDto(400, "Company Not Found! or already exist",0, new ArrayList<>());
         }
     }
 
